ea091623c2f8d40b7c4690df86395b432311d588
Omar Abdelhamid <Omar.Abdelhamid@geidea.net>
2023-11-20 13:07:21 +0200

    Seed Temp Data to CouponTB

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -8,6 +8,24 @@ namespace Mango.Services.CouponAPI.Data
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
         public DbSet<Coupon> Coupons { get; set; }
 
-       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "10OFF",
+                DicountAmount = 10,
+                MinAmount = 20,
+            });
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "20OFF",
+                DicountAmount = 20,
+                MinAmount = 30,
+            });
+        }
+
     }
 }
