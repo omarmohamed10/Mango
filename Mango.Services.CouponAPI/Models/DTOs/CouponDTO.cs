27d01d3a02ea892fa5b4402b5550e6dc1a1c0707
Omar Abdelhamid <Omar.Abdelhamid@geidea.net>
2023-11-20 13:01:25 +0200

    Add project files.

---
+++ Mango.Services.CouponAPI/Models/DTOs/CouponDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models.DTOs
+{
+    public class CouponDTO
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DicountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
---
+++ Mango.Services.CouponAPI/Program.cs
@@ -0,0 +1,48 @@
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+ApplyMigration();
+
+app.Run();
+
+
+void ApplyMigration()
+{
+    using(var scope = app.Services.CreateScope())
+    {
+        var db_service = scope.ServiceProvider.GetService<AppDbContext>();
+        if(db_service.Database.GetPendingMigrations().Count() > 0)
+        {
+            db_service.Database.Migrate();
+        }
+    }
+}
